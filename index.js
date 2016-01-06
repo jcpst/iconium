bd711fdb5c62be1c8852c552da6b35b4534f21d5
Joseph Post <jp@jsphpst.com>
2016-01-06 00:22:02 -0600

    start adding run commands

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+#!/usr/bin/env node
+
 require('shelljs/global')
 
 var crypto = require('crypto')
+var fs = require('fs')
 var path = require('path')
+var EOL = require('os').EOL
+var Docker = require('dockerode')
+var yaml = require('js-yaml')
 var packageJson = require('./package.json')
-var privateToken = path.join(__dirname, 'kite.aes')
+
+var privateToken = path.join(__dirname, 'codeship.aes')
+var services, steps
 
 function generateAesKey(file) {
   crypto.randomBytes(32).toString('base64').to(file)
@@ -12,30 +20,62 @@ function generateAesKey(file) {
 function encrypt(text, token) {
   var cipher = crypto.createCipher('aes-256-cbc', cat(token))
   var crypted = cipher.update(text,'utf8','base64')
-  crypted += cipher.final('base64');
+  crypted += cipher.final('base64')
   return crypted
 }
 
 function decrypt(text, token) {
   var decipher = crypto.createDecipher('aes-256-cbc', cat(token))
   var dec = decipher.update(text,'base64','utf8')
-  dec += decipher.final('utf8');
-  return dec;
+  dec += decipher.final('utf8')
+  return dec
+}
+
+
+// TODO: STEPS
+// x load yaml
+// . run service based on yml config
+// . call service by step
+// . run each step
+function dockerBuild() {}
+function runStep(stepName) {}
+function runService() {}
+
+function loadYaml(file) {
+  try {
+    return yaml.safeLoad(fs.readFileSync(file, 'utf8'))
+  } catch (e) {
+    console.log('didnt work: ' + e)
+    return ''
+  }
+}
+
+function steps(serviceFile, stepsFile) {
+  services = loadYaml(serviceFile)
+  steps = loadYaml(stepsFile)
+  console.log(services.maven)
+  console.log()
+  console.log(steps)
 }
 
 //-----------------------------------------------------------------------------
 // CLI Configuration
 //-----------------------------------------------------------------------------
-
 var argv = require('yargs')
-  .usage('Usage: $0 <command> [options]')
+  .usage('Usage:' + EOL + '  $0 <command> [options]')
   
+  .command('version', "", function () {
+    console.log(packageJson.version) 
+  })
+
+  //---------------------------------------------------------------------------
+  // Crypto.
+  //---------------------------------------------------------------------------
   .command('generate', 'Generate new private token.', function (yargs) {
     argv = yargs.option('f', {
       alias: 'file',
-      default: 'kite.aes',
-      description: 'Filename to write key to.'
-    })
+      default: 'codeship.aes',
+      description: 'Filename to write key to.' })
     .help('h')
     .alias('h', 'help')
     .argv
@@ -46,16 +86,13 @@ var argv = require('yargs')
     argv = yargs
       .option('i', {
         alias: 'input',
-        description: 'File to encrypt.'
-      })
+        description: 'File to encrypt.' })
       .option('o', {
         alias: 'output',
-        description: 'Name of output file.'
-      })
+        description: 'Name of output file.' })
       .option('t', {
         alias: 'token',
-        description: 'Path to your token file'
-      })
+        description: 'Path to your token file' })
       .demand(['i', 'o'])
       .help('h')
       .alias('h', 'help')
@@ -66,18 +103,15 @@ var argv = require('yargs')
   
   .command('decrypt', 'Decrypt a file.', function (yargs) {
     argv = yargs
-      .option('i', {
+      .option('i', { 
         alias: 'input',
-        description: 'File to decrypt.'
-      })
+        description: 'File to decrypt.' })
       .option('o', {
         alias: 'output',
-        description: 'Name of output file.'
-      })
+        description: 'Name of output file.' })
       .option('t', {
         alias: 'token',
-        description: 'Path to your token file'
-      })
+        description: 'Path to your token file' })
       .demand(['i', 'o'])
       .help('h')
       .alias('h', 'help')
@@ -86,9 +120,25 @@ var argv = require('yargs')
     decrypt(cat(argv.input), token).to(argv.output)
   })
 
-  .example('$0 generate -f kite.aes', 'Create new token file')
+  //---------------------------------------------------------------------------
+  // Build.
+  //---------------------------------------------------------------------------
+  .command('steps', 'Run your steps.', function (yargs) {
+    argv = yargs.help('h').alias('h', 'help').argv
+    steps('codeship-services.yml', 'codeship-steps.yml')
+  })
+
+  .command('run', 'Run a docker service.', function (yargs) {
+    argv = yargs.help('h').alias('h', 'help').argv 
+  })
+
+  .command('load', 'Pull an image or build a docker service.', function (yargs) {
+    argv = yargs.help('h').alias('h', 'help').argv 
+  })
+
+  .default('h')
   .help('h')
   .alias('h', 'help')
-  .epilog('Version:  ' + packageJson.version)
+  .epilog('Use "kite [command] (-h|--help)" for more information about a command.')
   .argv
 
